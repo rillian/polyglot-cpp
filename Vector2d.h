05391642b7c88f359a8aa8b7e2bb4e3856b138e1
Ralph Giles <giles@mozilla.com>
2015-05-22 11:08:38 -0700

    Scalar multiplication.

--- Vector2d.h
+++ Vector2d.h
@@ -63,6 +63,12 @@ class Vector2d {
       return *this;
     }
 
+    Vector2d& operator-=(const Vector2d& rhs) {
+      _x -= rhs._x;
+      _y -= rhs._y;
+      return *this;
+    }
+
   private:
     type _x = 0;
     type _y = 0;
@@ -74,11 +80,29 @@ Vector2d operator+(const Vector2d& lhs, const Vector2d& rhs) {
   return result += rhs;
 }
 
+Vector2d operator-(const Vector2d& lhs, const Vector2d& rhs) {
+  auto result = lhs;
+  return result -= rhs;
+}
+
+Vector2d operator*(const Vector2d& lhs, const Vector2d::type rhs) {
+  auto result = lhs;
+  result.setX(lhs.x() * rhs);
+  result.setY(lhs.y() * rhs);
+  return result;
+}
+
+Vector2d operator*(const Vector2d::type lhs, const Vector2d& rhs) {
+  auto result = rhs;
+  result.setX(lhs * rhs.x());
+  result.setY(lhs * rhs.y());
+  return result;
+}
+
 auto v = Vector2d{2,3,4};
 auto u = Vector2d::buildWithAngleAndMagnitude(M_PI, 1);
 void f() {
-  u = v;
-  u.x();
+  auto w = 3 * (u + v) * 8;
 }
 
 } // namespace poly
