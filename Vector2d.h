dabf4ffbfbdd8afa8c083af6c28ccf5e4818d727
Ralph Giles <giles@mozilla.com>
2015-05-22 10:46:38 -0700

    ctor examples.
    Some of this doesn't compile with deleted dtor errors.

--- Vector2d.h
+++ Vector2d.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <initializer_list>
+#include <cmath>
 
 namespace poly {
 
@@ -21,6 +22,22 @@ class Vector2d {
         _y = *iter;
       }
     }
+
+#if 0
+    Vector2d(const Vector2d& v) = delete; // copy ctor
+    Vector2d(const Vector2d&& v) = default; // move ctor
+
+    ~Vector2d() { // dtor
+      std::cout << "vector destroyed\n";
+    }
+
+    Vector2d& operator=(const Vector2d& v) = default;
+
+#endif
+    static Vector2d buildWithAngleAndMagnitude(type angle, type magnitude) {
+      return Vector2d{magnitude * cos(angle), magnitude * std::sin(angle)};
+    }
+
     // accessors
 
     // operators
@@ -31,5 +48,9 @@ class Vector2d {
 };
 
 auto v = Vector2d{2,3,4};
+auto u = Vector2d::buildWithAngleAndMagnitude(M_PI, 1);
+void f() {
+  u = v;
+}
 
 } // namespace poly
