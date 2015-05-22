03389cac1a4ed6eabb6233931863d73fccca274d
Ralph Giles <giles@mozilla.com>
2015-05-22 09:14:28 -0700

    Make fib compile on its own.

--- fib.cpp
+++ fib.cpp
@@ -1,9 +1,14 @@
+#include <array>
+
 // C++14 lets us use multiple return statements in a constexpr.
 inline constexpr auto fib(int n) noexcept {
   if (n == 0 || n == 1) {
     return 1;
   }
-  return fib(n-1) + fin(n-2);
+  return fib(n-1) + fib(n-2);
 }
 
-std::array<int, fib(4)> a;
+int main() {
+  std::array<int, fib(4)> a;
+  return 0;
+}
